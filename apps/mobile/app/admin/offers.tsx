da181dac9612dc9743a9b76e2f9a90717f94a8a5
mrbabu07 <mdjahedulislamjaved@gmail.com>
2026-08-14 17:19:28 +0600

    feat: complete admin promotional offers

--- apps/mobile/app/admin/offers.tsx
+++ apps/mobile/app/admin/offers.tsx
@@ -1,2 +1,2 @@
-import { AdminCommerceWorkspaceScreen } from "../../src/features/admin/AdminCommerceWorkspaceScreen";
-export default function Route() { return <AdminCommerceWorkspaceScreen kind="offers" />; }
+import { AdminOffersScreen } from "../../src/features/admin/AdminOffersScreen";
+export default function Route() { return <AdminOffersScreen />; }
